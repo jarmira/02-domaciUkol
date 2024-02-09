79f3300ee453fb3cc285b0d3397ddccf09f195c2
jarmira <134557583+jarmira@users.noreply.github.com>
2024-02-09 23:00:06 +0100

    snehulak

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -15,24 +15,49 @@ public class HlavniProgram {
         //TODO Tady bude kód pro kreslení želví grafiky.
 
 //zmrzlina
-nakresliKrug(100, Color.blue);
-
-zofka.penUp();
-zofka.turnRight(180);
- for (int i = 0; i <3 ; i++) {
-   zofka.move(2*0.08715574274*100);
-   zofka.turnLeft(10);
-        }
-zofka.penDown();
- zofka.turnLeft(90);
- nakresliTreuhelnik(180,  Color.ORANGE);
+//nakresliKrug(100, Color.blue);
+//
+//zofka.penUp();
+//zofka.turnRight(180);
+// for (int i = 0; i <3 ; i++) {
+//   zofka.move(2*0.08715574274*100);
+//   zofka.turnLeft(10);
+//        }
+//zofka.penDown();
+// zofka.turnLeft(90);
+// nakresliTreuhelnik(180,  Color.ORANGE);
 
 //snehulak
-
-
+nakresliKrug(50, Color.CYAN);
+otockaKolo(50);
+nakresliKrug(80, Color.blue);
+otockaKolo(80);
+nakresliKrug(20,Color.cyan);
+zofka.turnLeft(75);
+zofka.penUp();
+zofka.move(160);
+zofka.turnLeft(90);
+zofka.penDown();
+nakresliKrug(20, Color.cyan);
+zofka.turnLeft(140);
+zofka.penUp();
+zofka.move(102);
+zofka.penDown();
+zofka.turnLeft(128);
+nakresliKrug(100, Color.blue);
     }
 
-private void nakresliKrug(double radiusStrany, Color barva) {
+    private void otockaKolo(double radiusStrany) {
+        zofka.penUp();
+        zofka.turnRight(180);
+        for (int i = 0; i <8 ; i++) {
+          zofka.move(2*0.08715574274*radiusStrany);
+          zofka.turnLeft(12);
+               }
+        zofka.penDown();
+    }
+
+    private void nakresliKrug(double radiusStrany, Color barva) {
    zofka.setPenColor(barva);
    for (int i = 0; i < 36; i++) {
    zofka.move(2*0.08715574274*radiusStrany);
