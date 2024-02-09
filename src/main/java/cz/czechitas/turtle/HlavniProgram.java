87820bd1eb136493be4dcc62092dcfead4b4034b
jarmira <134557583+jarmira@users.noreply.github.com>
2024-02-09 22:08:28 +0100

    Prvni ukol Cast2

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -14,47 +14,52 @@ public class HlavniProgram {
     public void start() {
         //TODO Tady bude kód pro kreslení želví grafiky.
 
-nakresliTreuhelnik(150, new Color(50,250,100));
+nakresliTreuhelnik(-150.23d, new Color(50,250,100));
+
 zofka.penUp();
 zofka.turnRight(180);
 zofka.move(200);
 zofka.penDown();
+
 nakresliCtverecek(200, Color.red);
+
 zofka.penUp();
 zofka.turnLeft(90);
 zofka.move(250);
 zofka.penDown();
 
-nakresliObdelnik(150, new Color(150,25,200));
+nakresliObdelnik(-100, 200,  new Color(150,25,200));
 
 zofka.penUp();
 zofka.move(300);
+zofka.turnLeft(90);
+zofka.move(300);
+zofka.turnRight(180);
 zofka.penDown();
 
-nakresliKrug(30, Color.red);
+nakresliKrug(200, Color.red);
     }
 
-private void nakresliKrug(int delkaStrany, Color barva) {
+private void nakresliKrug(double radiusStrany, Color barva) {
    zofka.setPenColor(barva);
-   for (int i = 0; i < 18; i++) {
-   zofka.move(delkaStrany);
-   zofka.turnRight(20);
+   for (int i = 0; i < 36; i++) {
+   zofka.move(2*0.08715574274*radiusStrany);
+   zofka.turnRight(10);
         }
     }
 
-    private void nakresliObdelnik(int delkaStrany, Color barva) {
+    private void nakresliObdelnik(double delkaStranyA,double delkaStranyB, Color barva) {
         zofka.setPenColor(barva);
-        var velikostPrepony = Math.sqrt(2*Math.pow(delkaStrany, 2));
         for (int i = 0; i <2 ; i++) {
-   zofka.move(delkaStrany);
+   zofka.move(delkaStranyA);
    zofka.turnLeft(90);
-   zofka.move(velikostPrepony);
+   zofka.move(delkaStranyB);
    zofka.turnLeft(90);
            }
     }
 
 
-    private void nakresliTreuhelnik(int delkaStrany, Color barva) {
+    private void nakresliTreuhelnik(double delkaStrany, Color barva) {
     zofka.setPenColor(barva);
     for (int i = 0; i < 3; i++) {
         zofka.turnRight(30);
@@ -63,7 +68,7 @@ private void nakresliKrug(int delkaStrany, Color barva) {
     }
 }
 
-public void nakresliCtverecek(int delkaStrany, Color barva) {
+public void nakresliCtverecek(double delkaStrany, Color barva) {
     zofka.setPenColor(barva);
     for (int i = 0; i < 4; i++) {
         zofka.move(delkaStrany);
